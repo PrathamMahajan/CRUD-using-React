67c41ddf61fc4bf16a72a331a2e513b8467ababc
Pratham <crce.9383.aids@gmail.com>
2023-06-26 02:47:11 +0530

    files added

---
+++ src/components/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada
+          odio id tristique aliquam. Phasellus consequat justo id odio mattis,
+          sed rutrum sem sollicitudin.
+        </p>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada
+          odio id tristique aliquam. Phasellus consequat justo id odio mattis,
+          sed rutrum sem sollicitudin.
+        </p>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada
+          odio id tristique aliquam. Phasellus consequat justo id odio mattis,
+          sed rutrum sem sollicitudin.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
