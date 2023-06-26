738274f02f9fc22309dafd0c425b03dfa2961705
Pratham <crce.9383.aids@gmail.com>
2023-06-26 18:28:48 +0530

    more changes

---
+++ src/components/users/User.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+
+const User = () => {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { id } = useParams();
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const res = await axios.get(`http://localhost:3003/users/${id}`);
+    setUser(res.data);
+  };
+
+  return (
+    <div className="container py-4">
+      <Link className="btn btn-primary" to="/">
+        back to Home
+      </Link>
+      <h1 className="display-4">User Id: {id}</h1>
+      <hr />
+      <ul className="list-group w-50">
+        <li className="list-group-item">name: {user.name}</li>
+        <li className="list-group-item">user name: {user.username}</li>
+        <li className="list-group-item">email: {user.email}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default User;
